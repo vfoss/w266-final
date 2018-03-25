9a6ce0ae7f1519effe08014af31cfc25d61a42e5
Matthew Adereth <adereth@berkeley.edu>
2018-03-25 20:35:14 +0000

    Computing PMI with Gender

--- data-download.sh
+++ data-download.sh
@@ -3,13 +3,13 @@ sudo apt install transmission-cli
 mkdir -p data
 cd data
 
-#From https://meta.wikimedia.org/wiki/Data_dump_torrents
+# Fetch and unzip Wikipedia
 wget http://itorrents.org/torrent/D567CE8E2EC4792A99197FB61DEAEBD70ADD97C0.torrent
-
 tmpfile=$(mktemp)
 chmod a+x $tmpfile
 echo "killall transmission-cli" > $tmpfile
-
 transmission-cli -f $tmpfile -w . D567CE8E2EC4792A99197FB61DEAEBD70ADD97C0.torrent
-
 bunzip2 -v enwiki-20170820-pages-articles.xml.bz2
+
+# Download gender labels
+wget http://www.cs.cmu.edu/~ark/bio/data/wiki.genders.txt
